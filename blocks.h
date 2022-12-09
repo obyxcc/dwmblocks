064da26efcaaa7967c35ae63897464a2aea8067e
obyx <cole@obyx.cc>
2022-12-08 23:08:25 -0500

    added blocks

--- blocks.h
+++ blocks.h
@@ -3,9 +3,10 @@ static const Block blocks[] = {
 	/*Icon*/	  /*Command*/		          /*Update Interval*/	/*Update Signal*/
 	{"",        "update-blk",			  		1800,		             3},
 	{"",        "battery-blk",					1,		               0},
-	{"",        "brightness-blk",			  1,		               0},
+	{"",        "brightness-blk",			  1,		              11},
 	{"",        "volume-blk",					  1,		              10},
 	{"",        "network-blk",					1,		               0},
+	/* {"",        "envy-blk",					    1,		               0}, */
 	{"",        "weather-blk",					1800,                0},
 	{"",        "date +'%l:%M%p   '",		5,		               0},
 };
