bebdd8a47216a56b3588568d0677ff214e636da3
cole@obyx.cc <cole@obyx.cc>
2023-08-13 16:16:35 -0400

    rearrange blocks

--- config.c
+++ config.c
@@ -5,18 +5,18 @@
 
 Block blocks[] = {
     {"bplayerctl",    1,  1 },
-    {"bplayerctl-previous", 1,    2 },
-    {"bplayerctl-play", 1,    3 },
-    {"bplayerctl-next", 1,    4 },
-    {"bupdate", 1800,    6 },
-    // {"battery-blk",   1,    2 },
+    // {"bplayerctl-previous", 1,    2 },
+    // {"bplayerctl-play", 1,    3 },
+    // {"bplayerctl-next", 1,    4 },
+    {"bupdate", 1800,    5 },
+    {"bvolume",   1,    6 },
     // {"brightness-blk",    1, 3 },
-    // {"volume-blk",    1, 4 },
-    // {"network-blk",  1,   5 },
-    {"bprintstatus",     1,    7 },
+    {"bnetwork",  1,   7 },
+    // {"bprintstatus",     1,    7 },
     // {"bscreenshot",    0,  11 },
-    {"bdate",     5,    8 },
+    {"bbattery",    1, 8 },
     {"bcompositor",    0,  9 },
+    {"bdate",     5,    10 },
     // {"bpower",     15,    5 },
 };
 
--- config.h
+++ config.h
@@ -3,4 +3,4 @@
 #define CLICKABLE_BLOCKS  1     // Enable clickability for blocks
 #define CMDLENGTH         150   // Trim block output to this length
 #define DELIMITER         "  "  // Delimiter string used to separate blocks
-#define LEADING_DELIMITER 1     // Whether a leading separator should be used
+#define LEADING_DELIMITER 0     // Whether a leading separator should be used
