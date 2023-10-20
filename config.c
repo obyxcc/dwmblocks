3032573d155fd2f46d4a7c69818479cb6455dc9b
cole@obyx.cc <cole@obyx.cc>
2023-10-20 16:51:06 -0400

    minor fixes

--- config.c
+++ config.c
@@ -4,20 +4,17 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"bplayerctl",    1,  1 },
+    // {"bdocker", 5,    3 },
+    // {"bplayerctl",    0,  1 },
     // {"bplayerctl-previous", 1,    2 },
     // {"bplayerctl-play", 1,    3 },
     // {"bplayerctl-next", 1,    4 },
     {"bupdate", 1800,    5 },
-    {"bvolume",   1,    6 },
-    // {"brightness-blk",    1, 3 },
-    {"bnetwork",  1,   7 },
-    // {"bprintstatus",     1,    7 },
-    // {"bscreenshot",    0,  11 },
-    {"bbattery",    1, 8 },
-    {"bcompositor",    0,  9 },
-    {"bdate",     5,    10 },
-    // {"bpower",     15,    5 },
+    // {"bmic",   0,    4 },
+    // {"bvolume",   0,    6 },
+    {"bbattery",    60, 8 },
+    {"bnetwork",  10,   7 },
+    {"bdate",     15,    10 },
 };
 
 const unsigned short blockCount = LEN(blocks);
